f957e7390f1e08704466d2aee522c1237fe038ee
Kyle Hargraves <pd@krh.me>
2010-05-20 17:55:41 -0500

    simplest possible thing right? ...

--- jquery.keybind.js
+++ jquery.keybind.js
@@ -1,11 +1,39 @@
 (function($) {
 
-$.fn.extend({
-  keybind: function(seq, handler) {
-  },
+  $.fn.extend({
+    keybind: function(seq, handler) {
+      var data = this.data('keybind');
 
-  keyunbind: function(seq, handler) {
+      if (!data) {
+        data = { bindings: [] };
+        this.data('keybind', data)
+            .bind({ keypress: keypressHandler,
+                    keydown:  keydownHandler,
+                    keyup:    keyupHandler });
+      }
+
+      data.bindings.push(handler);
+
+      return this;
+    },
+
+    keyunbind: function(seq, handler) {
+      return this;
+    }
+  });
+
+  function keypressHandler(event) {
+    var data = $(this).data('keybind');
+    $.each(data.bindings, function(i, fn) {
+      fn({chord:'a'}, event);
+    });
+    return false;
   }
-});
+
+  function keydownHandler(event) {
+  }
+
+  function keyupHandler(event) {
+  }
 
 }(jQuery));

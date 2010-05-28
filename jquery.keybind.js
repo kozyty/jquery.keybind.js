0589a10931987c618ffe994fbe3f64578da64e61
Kyle Hargraves <pd@krh.me>
2010-05-28 00:47:03 -0500

    add _specialKeys entries which are fully portable
    supposedly anyhow; they are green on http://unixpapa.com/js/key.html

--- jquery.keybind.js
+++ jquery.keybind.js
@@ -167,7 +167,11 @@
   }
 
   var _specialKeys = {
-    13: 'Enter', 27: 'Esc', 37: 'Left', 38: 'Up', 39: 'Right', 40: 'Down',
+    8: 'Backspace', 9: 'Tab', 13: 'Enter', 27: 'Esc',
+    32: 'Space', 33: 'PageUp', 34: 'PageDown', 35: 'End', 36: 'Home',
+    37: 'Left', 38: 'Up', 39: 'Right', 40: 'Down', 45: 'Insert', 46: 'Del',
+    112: 'F1', 113: 'F2', 114: 'F3', 115: 'F4', 116: 'F5', 117: 'F6',
+    118: 'F7', 119: 'F8', 120: 'F9', 121: 'F10', 122: 'F11', 123: 'F12',
     187: '=', 189: '-'
   };
 
